7e117aaecfa40bfb955a8e302c2c1dacafc2eb89
Brian Gardner <bgardner@bignerdranch.com>
2020-10-20 14:40:20 -0400

    Add comment explaining remote and local messages

--- BluetoothLeChat/app/src/main/java/com/example/bluetoothlechat/bluetooth/Message.kt
+++ BluetoothLeChat/app/src/main/java/com/example/bluetoothlechat/bluetooth/Message.kt
@@ -15,6 +15,13 @@
  */
 package com.example.bluetoothlechat.bluetooth
 
+/**
+ * This sealed class represents the messages sent between connected devices.
+ * The RemoteMessage class represents a message coming from a remote device.
+ * The LocalMessage class represents a message the user wants to send to the remote device.
+ *
+ * @param text is the message text the user sends to the other connected device.
+ */
 sealed class Message(val text: String) {
     class RemoteMessage(text: String) : Message(text)
     class LocalMessage(text: String) : Message(text)
