7d01e81ab4c32121a12d2e1af5520b6e9bbf59d2
falconsmilie <heizenburgblue@gmail.com>
2020-11-29 20:42:03 +0100

    BluetoothLeGatt-UpgradeCleanUp: generify attributes variable

--- BluetoothLeGatt/Application/src/main/java/com/example/android/bluetoothlegatt/SampleGattAttributes.java
+++ BluetoothLeGatt/Application/src/main/java/com/example/android/bluetoothlegatt/SampleGattAttributes.java
@@ -22,7 +22,7 @@ import java.util.HashMap;
  * This class includes a small subset of standard GATT attributes for demonstration purposes.
  */
 public class SampleGattAttributes {
-    private static HashMap<String, String> attributes = new HashMap();
+    private static final HashMap<String, String> attributes = new HashMap<>();
     public static String HEART_RATE_MEASUREMENT = "00002a37-0000-1000-8000-00805f9b34fb";
     public static String CLIENT_CHARACTERISTIC_CONFIG = "00002902-0000-1000-8000-00805f9b34fb";
 
