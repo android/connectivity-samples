ed4a2aaca353313b1113dbb65410816ff7bafd79
Carlos Vinueza <chinesco@google.com>
2022-09-02 15:35:29 -0700

    Adding basic compose elements for control and send usecases
    Change-Id: If4324fde531b60b6ca641366c3f3bd58d7e210b7

--- UwbRanging/app/src/main/java/com/google/location/nearby/apps/hellouwb/ui/control/ControlScreen.kt
+++ UwbRanging/app/src/main/java/com/google/location/nearby/apps/hellouwb/ui/control/ControlScreen.kt
@@ -1,6 +1,48 @@
+
 package com.google.location.nearby.apps.hellouwb.ui.control
 
+import android.graphics.Color
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.uwb.RangingMeasurement
+import androidx.core.uwb.RangingPosition
+import com.google.location.nearby.apps.hellouwb.ui.home.ConnectedEndpoint
+import com.google.location.nearby.apps.hellouwb.ui.home.HomeScreen
+import com.google.location.nearby.apps.hellouwb.ui.home.HomeUiState
+import com.google.location.nearby.apps.uwbranging.UwbEndpoint
 
-@Composable fun ControlScreen(modifier: Modifier = Modifier) {}
+@Composable fun ControlScreen(modifier: Modifier = Modifier) {
+
+    Column(modifier = Modifier.padding(40.dp)) {
+        Text(text = "Switch")
+        Button(onClick = {
+            //your onclick code here
+        }) {
+            Text(text = "  On  ")
+        }
+
+        Button(onClick = {
+            //your onclick code here
+        }) {
+            Text(text = "  Off  ")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewControlScreen(modifier: Modifier = Modifier) {
+    ControlScreen(
+        modifier = modifier
+    )
+}
+
+
+
--- UwbRanging/app/src/main/java/com/google/location/nearby/apps/hellouwb/ui/send/SendScreen.kt
+++ UwbRanging/app/src/main/java/com/google/location/nearby/apps/hellouwb/ui/send/SendScreen.kt
@@ -1,6 +1,34 @@
 package com.google.location.nearby.apps.hellouwb.ui.send
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.google.location.nearby.apps.hellouwb.ui.control.ControlScreen
 
-@Composable fun SendScreen(modifier: Modifier = Modifier) {}
+@Composable fun SendScreen(modifier: Modifier = Modifier) {
+
+    Column(modifier = Modifier.padding(40.dp)) {
+        Text(text = "Point to share file")
+        Button(onClick = {
+            //your onclick code here
+        }) {
+            Text(text = "Share file")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewSendScreen(modifier: Modifier = Modifier) {
+    SendScreen(
+        modifier = modifier
+    )
+}
+
+
+
