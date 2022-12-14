8c5252f7cdc19ea234920c89765d53d682c429ab
Carlos Vinueza <chinesco@google.com>
2022-12-14 14:11:06 -0800

    Updating to latest dependencies and better permission management
    Change-Id: I1bbb65ae38ed8d74e46484c5c9d94f96707d75ce

--- BluetoothLeChat/app/src/main/java/com/example/bluetoothlechat/MainActivity.kt
+++ BluetoothLeChat/app/src/main/java/com/example/bluetoothlechat/MainActivity.kt
@@ -15,15 +15,29 @@
  */
 package com.example.bluetoothlechat
 
+import android.Manifest
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import com.example.bluetoothlechat.bluetooth.ChatServer
 
-class MainActivity : AppCompatActivity() {
+private const val TAG = "BluetoothLeChat"
 
+class MainActivity : AppCompatActivity() {
+    
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("BluetoothLeChat", "Requesting needed permissions")
+        requestPermissions.launch(arrayOf(
+            Manifest.permission.BLUETOOTH_ADVERTISE,
+            Manifest.permission.BLUETOOTH_CONNECT,
+            Manifest.permission.BLUETOOTH_SCAN
+            ))
     }
 
     // Run the chat server as long as the app is on screen
@@ -36,4 +50,11 @@ class MainActivity : AppCompatActivity() {
         super.onStop()
         ChatServer.stopServer()
     }
+
+    private val requestPermissions =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            permissions.entries.forEach {
+                Log.d(TAG, "${it.key} = ${it.value}")
+            }
+        }
 }
